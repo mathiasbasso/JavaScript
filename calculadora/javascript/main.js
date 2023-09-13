d735b8149b44a45899bd59286876654a5b01cae4
mathiasbasso <mathias201711@gmail.com>
2023-09-13 20:58:32 -0300

    finalizando

--- calculadora/javascript/main.js
+++ calculadora/javascript/main.js
@@ -1,1 +1,40 @@
-.
+//CAMPO DO VISOR DA CALCULADORA
+var screen = document.querySelector('#screen');
+
+//FUNÇÃO QUE LIMPA O VIOR DA CALCULADORA
+document.querySelector('#limpar').addEventListener('click', function(){
+    screen.value = '';
+});
+
+//FUNÇÃO QUE DELETA VALORES DA TELA DA CALCULADORA
+    document.querySelector('#deletar').addEventListener('click', function(){
+        screen.value = screen.value.substring(0, (screen.value.length - 1));
+    });
+
+//FUNÇÃO QUE ADICIONA AÇÃO NA TELA DOS BOTÕES
+function addToScreen(value){
+    const operadoresPadrao = ['*', '+', '-', '/', '.'];
+    screen.value += value;
+
+    if (operadoresPadrao.includes(value)){
+        var valorAnterior = screen.value.length -2;
+        if(operadoresPadrao.includes(screen.value[valorAnterior])){
+            screen.value = screen.value.substring(0, (screen.value.length - 2));
+            screen.value = screen.value + value;
+        }   
+    }
+    switch(screen.value[0]){
+        case '*':
+            screen.value = '';
+        case '/':
+            screen.value = '';
+        case '.':
+            screen.value = '';
+        case '-':
+            screen.value = '';
+
+    }
+}
+function result(){
+    screen.value = eval(screen.value);
+}

839777e5f9c974bc490c0059bfd64d89db91c72d
mathiasbasso <mathias201711@gmail.com>
2023-09-20 20:41:31 -0300

    atividades

---
+++ atividadesDesafio/desafio1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int x;
+        System.out.println("Qual é a sua idade em anos?");
+        x = entrada.nextInt();
+
+        System.out.println("Sua idade em anos: " + x);
+        System.out.println("Sua idade em meses: " + x * 12);
+        System.out.println("Sua idade em dias: " + x * 365);
+    }
+}

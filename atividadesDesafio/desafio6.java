f02bfa5378ac0fb9cc9c8e6b9820a50b8e4ec63b
mathiasbasso <mathias201711@gmail.com>
2023-09-20 21:54:12 -0300

    .....

---
+++ atividadesDesafio/desafio6.java
@@ -0,0 +1,29 @@
+package atividadesDesafio;
+
+public class desafio6 {
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+       int valor[];
+       int par, impar, negativo, positivo;
+       par= 0;
+       impar = 0;
+       negativo =0;
+       positivo=0;
+        for (int i= 0; i<5; i++){
+            if (valor[i] %2 == 0){
+                par++;
+            }if (valor[i]%2 !=0){
+                impar++;
+            }if 
+        }
+    }
+
+}
+
+}

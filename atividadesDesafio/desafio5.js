3d8b62483abb39a75c5b7f5c164a102dc55a41a6
mathiasbasso <mathias201711@gmail.com>
2023-09-20 21:20:12 -0300

    atualizaaandoo

---
+++ atividadesDesafio/desafio5.js
@@ -0,0 +1,8 @@
+var nome, valorHora, horaTrabalho;
+const salario = valorHora*horaTrabalho;
+
+nome = prompt("Digite o nome")
+valorHora = prompt("Valor para hora trabalhada")
+horaTrabalho = prompt("Quantas horas trabalhou? ")
+
+alert("O salario será de: " +salario+ " reais")

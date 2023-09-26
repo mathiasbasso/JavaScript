c2fb609ea72ed8cc33a981dd313669253ef37f4c
mathiasbasso <mathias201711@gmail.com>
2023-09-26 20:31:33 -0300

    ...

--- atividadesDesafio/desafio8.js
+++ atividadesDesafio/desafio8.js
@@ -9,24 +9,22 @@ python = prompt("voce estuda python");
 nome = prompt("digite seu nome");
 idade = prompt("Digite sua idade");
 
-if(idade > 18){
+if(idade >= 18){
         
 alert("vip (1) e padrão(2)");
 escolha = prompt("digite sua escolha");
     if (escolha ==1){
-       alert("o valor do ingreso é:" + entradaVIP);
              if(python  == 1){
                 entradaVIP = entradaVIP/2;
-                alert("vc tem desconto de 50% e valor do ingreso é:" + entradaVIP );
-                     }else{
-                     alert("voce nao tem desconto");}
-    }else (escolha ==2);{
-       entradaPadrao = entradaPadrao/2;
-        alert("o valor do ingreso é:" + entradaPadrao);
+                alert("vc tem desconto de 50% e valor do ingreso é: " + entradaVIP );
+              }else{
+                     alert("voce nao tem desconto e o ingresso custa: "+ entradaVIP);}
+    }else if (escolha ==2){
             if(python  == 1){
-            alert("vc tem desconto de 50%");
+                entradaPadrao = entradaPadrao/2;
+                alert("voce tem desconto e o valor do ingreso é:" + entradaPadrao);
             }else{
-                alert("voce nao tem desconto");
+                alert("voce nao tem desconto e o ingresso custa: "+ entradaPadrao);
              }
     }
 }else {
